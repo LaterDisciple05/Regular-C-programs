c36eb8d4d9532d060cc4c60df4ef5238c28c2d06
LaterDisciple05 <akshatshah11111@gmail.com>
2024-12-09 23:52:06 +0530

    details added to bitwiseoperators program

--- bitwiseoperators.c
+++ bitwiseoperators.c
@@ -12,8 +12,8 @@ int main()
  scanf("%d",&b);
  printf("a & b = %d\n",a &b);
  printf("a | b = %d\n",a |b);
- printf("a ^ b = %d\n",a ^b);
- printf("~a = %d\n",~a);
+ printf("a ^ b(exclusively or) = %d\n",a ^b);
+ printf("~a (one's complement)= %d\n",~a);
  printf("~b = %d\n",~b);
  printf("a<<2 = %d\n",a<<2);
  printf("b<<2 = %d\n",b<<2);
