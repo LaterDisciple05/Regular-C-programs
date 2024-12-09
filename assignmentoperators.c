0ee9f4278e0ebf08040d46d6609cb1a45b011dec
LaterDisciple05 <akshatshah11111@gmail.com>
2024-12-09 23:57:52 +0530

    added assignmentoperators.c

---
+++ assignmentoperators.c
@@ -0,0 +1,19 @@
+/*Program purpose:-use of assignment operators
+Program date:-09/12/2024
+Program author:-Akshat Shah
+Program flaws(if any):-NA*/
+#include<stdio.h>
+int main()
+{
+  int a,b;
+ printf("Enter number a=");
+ scanf("%d",&a);
+ printf("Enter number b=");
+ scanf("%d",&b);
+ printf("a = b = %d\n",a =b);
+ printf("a += b = %d\n",a +=b);
+ printf("a -= b = %d\n",a -=b);
+ printf("a *= b = %d\n",a *=b);
+ printf("a /= b = %d\n",a /=b);
+return 0;
+}
