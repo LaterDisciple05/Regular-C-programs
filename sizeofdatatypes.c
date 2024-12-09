326369e14e7011b16b0285cf050fc33d72dc5cba
LaterDisciple05 <akshatshah11111@gmail.com>
2024-12-09 23:10:10 +0530

    added sizeofdatatypes.c

---
+++ sizeofdatatypes.c
@@ -0,0 +1,13 @@
+/*Program purpose:-to find size of primary data types in c
+Program date:-09/12/2024
+Program author:-Akshat Shah
+Program flaws(if any):-NA*/
+#include<stdio.h>
+int main()
+{
+ printf("size of int=%lu\n",sizeof(int));
+ printf("size of char=%lu\n",sizeof(char));
+ printf("size of float=%lu\n",sizeof(float));
+ printf("size of double=%lu\n",sizeof(double));
+return 0;
+}
