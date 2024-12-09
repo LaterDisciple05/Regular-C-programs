45a17ea1ba3e49a1f72ef9c6c698fdee40d6f3d7
LaterDisciple05 <akshatshah11111@gmail.com>
2024-12-09 22:38:04 +0530

    add.c file added

---
+++ add.c
@@ -0,0 +1,17 @@
+/*Program purpose:-add 2 numbers
+Program date:-09/12/2024
+Program author:-Akshat Shah
+Program flaws(if any):-NA*/
+#include<stdio.h>
+int main()
+{
+ int a,b;
+ printf("Enter 1st number=");
+ scanf("%d",&a);
+ printf("Enter 2nd number=");
+ scanf("%d",&b);
+ printf("\nsum of both numbers is=%d",a+b);
+ 
+return 0;
+}
+
