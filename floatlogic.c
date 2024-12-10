86e5eb7c86f30bdd4e832746dd3a2283288a1422
LaterDisciple05 <akshatshah11111@gmail.com>
2024-12-10 12:15:50 +0530

    added floatlogic.c

---
+++ floatlogic.c
@@ -0,0 +1,15 @@
+/*Program purpose:-understand few of the print logics of float
+Program date:-10/12/24
+Program author:-Akshat Shah
+Program flaws(if any):-NA*/
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    float b=6.098998;
+ printf("Hello World!!\n");
+ printf("%8.3f\n",b);
+ printf("%-8.3f\n",b);
+
+return 0;
+}
