e221742c4e7d002131f38029ffc29d2561af1024
LaterDisciple05 <akshatshah11111@gmail.com>
2024-12-10 22:44:46 +0530

    added loops.c

---
+++ loops.c
@@ -0,0 +1,43 @@
+/*Program purpose:-using and understanding 3 types of loops in c (for,while,do while)
+Program date:-
+Program author:-Akshat Shah
+Program flaws(if any):-NA*/
+// #include<stdio.h>
+// int main()
+// {
+//  int n;
+// printf("Enter number n=");
+// scanf("%d",&n);
+//  for(int i=0;i<n;i++)
+//  {
+//     printf("%d\n",i);
+//  }
+// return 0;
+// }
+// #include<stdio.h>
+// int main()
+// {
+//  int n,i=0;
+// printf("Enter number n=");
+// scanf("%d",&n);
+//  while(i<n)
+//  {
+//     printf("%d\t",i);
+//     i++;
+//  }
+// return 0;
+// }
+#include<stdio.h>
+int main()
+{
+ int n,i=0;
+ printf("Enter number n=");
+ scanf("%d",&n);
+ do
+ {
+   printf("%d",i);
+   i++;
+ } while (i<n);
+ 
+return 0;
+}
