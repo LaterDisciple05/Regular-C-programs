41449e76c2f0bf110316ad28797365288ac27621
LaterDisciple05 <akshatshah11111@gmail.com>
2024-12-11 09:48:41 +0530

    added typecast.c

---
+++ typecast.c
@@ -0,0 +1,13 @@
+/*Program purpose:-to know logic of typecast
+Program date:-11/12/2024
+Program author:-Akshat Shah
+Program flaws(if any):-NA*/
+#include<stdio.h>
+int main()
+{
+ int a=5;
+ float b=3.09;
+ printf("number a=%d\n",a);
+ printf("number b=%d\n",(int)b);
+ return 0;
+}
