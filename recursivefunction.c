6c8c44129d24bc89f65e35a6b391a9bfe69a756b
LaterDisciple05 <akshatshah11111@gmail.com>
2024-12-14 12:03:13 +0530

    added recursivefunctions.c

---
+++ recursivefunction.c
@@ -0,0 +1,27 @@
+/*Program purpose:-Understanding recursive functions(tower of hanoi)
+Program date:-14/12/2024
+Program author:-Akshat Shah
+Program flaws(if any):-NA*/
+#include<stdio.h>
+int tower(int a);
+int main()
+{
+    int a,s;
+    printf("Enter range till which you want tower of hanoi:-");
+    scanf("%d",&a);
+    s=tower(a);
+    printf("%d",s);
+ 
+return 0;
+}
+int tower(int a)
+{
+  if(a==3)
+  {
+    return 7;
+  }
+  else
+  {
+    return (a-2)*7+1;
+  }
+}
