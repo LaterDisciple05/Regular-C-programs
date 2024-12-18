fcbd1b6c1eea604a28d3021f2eed47320a648ab1
LaterDisciple05 <akshatshah11111@gmail.com>
2024-12-18 12:46:16 +0530

    tower of hanoi program added

--- recursivefunction.c
+++ recursivefunction.c
@@ -3,25 +3,28 @@ Program date:-14/12/2024
 Program author:-Akshat Shah
 Program flaws(if any):-NA*/
 #include<stdio.h>
-int tower(int a);
+void tower(int n, char from_rod, char Helping_rod, char to_rod);
 int main()
 {
-    int a,s;
-    printf("Enter range till which you want tower of hanoi:-");
-    scanf("%d",&a);
-    s=tower(a);
-    printf("%d",s);
- 
+    int n,s;
+    printf("Enter number of disks involved in tower of hanoi:-");
+    scanf("%d",&n);
+    printf("Sequence of steps you need to Follow to solve!!\n");
+    tower(n,'A','B','C');
+
 return 0;
 }
-int tower(int a)
+void tower(int n, char from_rod, char Helping_rod, char to_rod)
 {
-  if(a==3)
+  if(n==1)
   {
-    return 7;
+    printf("Move Disk %d From Rod %c to Rod %c\n",n,from_rod,to_rod);
+    return;
   }
   else
   {
-    return tower(a-1)*2 +1;
-  }
+    tower(n - 1, from_rod, to_rod, Helping_rod);
+    printf("Move Disk %d From Rod %c to Rod %c\n",n,from_rod,to_rod);
+    tower(n - 1, Helping_rod, from_rod, to_rod);
+ }
 }
