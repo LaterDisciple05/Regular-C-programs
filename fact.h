cbb6022f395967470060eadf072f82d835b86140
LaterDisciple05 <akshatshah11111@gmail.com>
2025-01-05 22:14:27 +0530

    fact.h added

---
+++ fact.h
@@ -0,0 +1,11 @@
+int fact(int a)
+{
+  if(a==1||a==0)
+  {
+    return 1;
+  }
+  else
+  {
+    return a*fact(a-1);
+  }
+}
--- structunion.c
+++ structunion.c
@@ -8,9 +8,15 @@ struct car
  float milage;
  char name[20]; 
 };
+union specs
+{
+    float tank;
+    int engine;
+};
 int main()
 {
   struct car c1,c2,m1,m2;
+  union specs t1,t2,e1,e2;
   printf("Enter name of your car=");
   getchar();
   fgets(c1.name,20,stdin);
@@ -21,5 +27,16 @@ int main()
   fgets(c2.name,20,stdin);
   printf("Enter milage of your car=");
   scanf("%s",&m2.milage);
+  printf("Enter tank capacity of your car=");
+  getchar();
+  scanf("%s",&t1.tank);
+  printf("Enter engine capacity of your car=");
+  scanf("%s",&e1.engine);
+  printf("Enter tank capacity of your car=");
+  getchar();
+  scanf("%s",&t2.tank);
+  printf("Enter engine capacity of your car=");
+  scanf("%s",&e2.engine);
+  //structures save everything and union saves only last entry
 return 0;
 }
